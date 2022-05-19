d2fd4fe73c701b58bf2c69eb26de858876f53835
IT19000886 <it19000886@my.sliit.lk>
2022-05-19 18:28:50 +0530

    Updated github action

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,9 @@ import { app } from './app';
 import mongoose from 'mongoose';
 
 const start = async () => {
-  const port = 8081;
-  if (!process.env.JWT_KEY) {
-    process.env.JWT_KEY = "secret550"
-    // throw new Error('JWT_KEY should be added as an environment variable');
-  }
+  const port = 8082;
   if (!process.env.MONGO_URI) {
-    process.env.MONGO_URI="mongodb+srv://cmt:admin123@cluster0.yjsmx.mongodb.net/cmt_testing123?retryWrites=true&w=majority"
+    process.env.MONGO_URI="mongodb://localhost:27017/ctse"
     // throw new Error('MONGO_URI should be added as an environment variable');
   }
  
